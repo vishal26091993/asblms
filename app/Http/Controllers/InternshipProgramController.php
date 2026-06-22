f0372f280aa8654a13a676837d8a9ac697b5ce8b
vishal26091993 <vishalpund24@gmail.com>
2026-06-22 12:56:41 +0530

    uploads
    uploads

---
+++ app/Http/Controllers/InternshipProgramController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InternshipProgram;
+use Illuminate\Http\Request;
+
+class InternshipProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InternshipProgram $internshipProgram)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InternshipProgram $internshipProgram)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InternshipProgram $internshipProgram)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InternshipProgram $internshipProgram)
+    {
+        //
+    }
+}
